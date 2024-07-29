93b2fd70f0c6a3e074cd1e9d840f84cd5dd94ce2
SrJasper <lulevita1@hotmail.com>
2024-07-29 19:23:53 -0300

    Revert "fix: image folders fix"
    This reverts commit

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -52,7 +52,7 @@ const Home: React.FC = () => {
       <div className="container-main-options">
         <div>
           <img
-            src="../../../assets/images/guy-Icon.png"
+            src="../../../public/assets/images/guy-Icon.png"
             // src="../../public/assets/images/guy-Icon.png"
             alt=""
             className="page-image"
@@ -60,12 +60,12 @@ const Home: React.FC = () => {
           <label className="page-text">{t("about-me")}</label>
         </div>
         <div className="hover" onClick={goToProjectsPage}>
-          <img src="../../../assets/images/pc.png" alt="" className="page-image" />
+          <img src="../../../public/assets/images/pc.png" alt="" className="page-image" />
           <label className="page-text">{t("projects")}</label>
         </div>
         <div>
           <img
-            src="../../../assets/images/Contact.png"
+            src="../../../public/assets/images/Contact.png"
             alt=""
             className="page-image"
           />
