1231f7bbf558abca16d0b59fb8378750a5f87752
SrJasper <lulevita1@hotmail.com>
2024-07-30 15:14:35 -0300

    fix: images to assets 5

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
-import React, { useEffect } from 'react';
-import './header.css';
+import React, { useEffect } from "react";
+import "./header.css";
 import { useTranslation } from "react-i18next";
 import { useState } from "react";
 
@@ -9,62 +9,65 @@ interface Props {
 }
 
 const Header: React.FC<Props> = ({ title }) => {
-
   const [language, setLanguage] = useState("");
   const { t, i18n } = useTranslation();
   useEffect(() => {
     i18n.changeLanguage(language);
   }, [language]);
 
-
   const navigate = useNavigate();
   const goToMainPage = () => {
-    if (title === 1) 
-      return;
+    if (title === 1) return;
 
     navigate("/");
   };
   const goToMePage = () => {
-    if (title === 2) 
-      return;
+    if (title === 2) return;
     navigate("/me");
   };
   const goToProjectsPage = () => {
-    if (title === 3) 
-      return;
+    if (title === 3) return;
     navigate("/projects");
   };
 
   return (
     <div className="header">
-      
-      <div className='header-options'>
-        <img className='../../../assets/images/header-img' src="/profile-pic.png" alt="Profile" />
-        <h1 className={`${title === 1 ? 'green' : ''}`} onClick={goToMainPage}>
+      <div className="header-options">
+        <img
+          src="../../../assets/images/profile-pic.png"
+          className="header-img"
+          alt="Profile"
+        />
+        <h1 className={`${title === 1 ? "green" : ""}`} onClick={goToMainPage}>
           {t("home-page") /* Página Inicial */}
         </h1>
-        <h1 className={`${title === 2 ? 'green' : ''}`} onClick={goToMePage}>
+        <h1 className={`${title === 2 ? "green" : ""}`} onClick={goToMePage}>
           {t("about-me") /* Sobre Mim */}
-        </h1>        
-        <h1 className={`${title === 3 ? 'green' : ''}`} onClick={goToProjectsPage}>
+        </h1>
+        <h1
+          className={`${title === 3 ? "green" : ""}`}
+          onClick={goToProjectsPage}
+        >
           {t("projects") /* Projetos */}
         </h1>
       </div>
 
       <button
-        className={`language-button ${localStorage.getItem("lng") === "en" ? "en-button" : "pt-button"}`}
+        className={`language-button ${
+          localStorage.getItem("lng") === "en" ? "en-button" : "pt-button"
+        }`}
         onClick={() => {
-        if(localStorage.getItem("lng") === "en"){
-          localStorage.setItem("lng", "pt");
-          setLanguage("pt");
-        } else {
-          localStorage.setItem("lng", "en");
-          setLanguage("en");
-        }
-      }}
+          if (localStorage.getItem("lng") === "en") {
+            localStorage.setItem("lng", "pt");
+            setLanguage("pt");
+          } else {
+            localStorage.setItem("lng", "en");
+            setLanguage("en");
+          }
+        }}
       ></button>
     </div>
   );
 };
 
-export {Header};
+export { Header };
